50722869145ce69431d8c13fd6aace0285647ca6
Andrew L. <qilip@outlook.com>
2015-02-21 20:31:12 -0500

    Beating heart created with css and some js.
    I plan to recreate all of this with two.js

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready( function () {
+
+	$('#heart').click(function() {
+		
+		var heartClass = $('#heart').attr("class");
+		switch(heartClass)
+		{
+		case 'pulse5':
+		 	$('#heart').removeClass('pulse5').addClass("pulse1");
+		 	$('#inner').html("It's Valentines day...");
+		 	break;
+
+		case 'pulse4':
+			$('#heart').removeClass('pulse4').addClass('pulse5');
+			$('#inner').html("with all of my heart.");
+			break;
+
+		case 'pulse3':
+			$('#heart').removeClass('pulse3').addClass('pulse4');
+			$('#inner').html("that I love you...");
+			break;
+
+		case 'pulse2':
+			$('#heart').removeClass('pulse2').addClass('pulse3');
+			$('#inner').html("you should know...");
+			break;
+
+		case 'pulse1':
+			$('#heart').removeClass('pulse1').addClass('pulse2');
+			$('#inner').html("so I figured...");
+			break;
+
+		case '':
+			$('#heart').addClass('pulse1');
+			$('#inner').html("It's Valentines day...");
+			break;
+		}
+	});
+});
